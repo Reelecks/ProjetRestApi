0452e3b4559aa7acfa71c78f0cee57f98964196c
Reelecks <robinlandais75@yahoo.com>
2023-10-18 14:27:50 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+const PORT = process.env.PORT || 3000;
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: 'biblio'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    console.log('Connecté à la base de données');
+});
+
+app.get("/", (req, res) => {
+  res.send("server ready");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
